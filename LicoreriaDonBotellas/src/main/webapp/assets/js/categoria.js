b4f7d3787ab5c5f50a96d4833883d5ad37b1eaad
Adrian Morales <adrianmoraleszevallos@gmail.com>
2024-10-27 20:24:14 -0500

    Controladores de categoria y usuarios

--- LicoreriaDonBotellas/src/main/webapp/assets/js/categoria.js
+++ LicoreriaDonBotellas/src/main/webapp/assets/js/categoria.js
@@ -4,11 +4,12 @@
  */
 
 
-fetch("http://localhost:8080/LicoreriaDonBotellas-1.0/UsuarioController")
+
+fetch("http://localhost:8080/LicoreriaDonBotellas-1.0/CategoriaController")
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        const tabla = document.getElementById('TablaUsuarioz');
+        const tabla = document.getElementById('TablaCategoriaz');
         tabla.innerHTML = ''; // Limpiar la tabla antes de actualizar
 
         // Crear y agregar el encabezado
@@ -17,29 +18,19 @@ fetch("http://localhost:8080/LicoreriaDonBotellas-1.0/UsuarioController")
             <tr>
                 <th>ID</th>
                 <th>Nombre</th>
-                <th>Email</th>
-                <th>Password</th>
-                <th>Direccion</th>
-                <th>Telefono</th>
-                <th>Rol</th>
-                <th>F.Creacion</th>
+                <th>Descripcion</th>
             </tr>
         `;
         tabla.appendChild(thead);
 
         // Crear el cuerpo de la tabla
         const tbody = document.createElement('tbody');
-        data.forEach(usuario => {
+        data.forEach(categoria => {
             const fila = document.createElement('tr');
             fila.innerHTML =
-                `<td>${usuario.userId}</td>` +
-                `<td>${usuario.nombre}</td>` +
-                `<td>${usuario.email}</td>` +
-                `<td>${usuario.password}</td>` +
-                `<td>${usuario.direccion}</td>` +
-                `<td>${usuario.telefono}</td>` +
-                `<td>${usuario.rol}</td>` +
-                `<td>${usuario.fecha_creacion || 'N/A'}</td>`; // Agregar fecha_caducidad si existe
+                `<td>${categoria.categoria_id}</td>` +
+                `<td>${categoria.nombre}</td>` +
+                `<td>${categoria.descripcion || 'N/A'}</td>`;  // Agregar fecha_caducidad si existe
             tbody.appendChild(fila); // Añadir la fila a tbody
         });
         tabla.appendChild(tbody); // Añadir el cuerpo a la tabla
